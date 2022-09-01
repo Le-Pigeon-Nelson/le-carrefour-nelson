9023e0328755b74fc9a244b510ea638ea25f3269
jeremyk6 <jeremy.kalsron@gmail.com>
2022-09-01 15:52:55 +0200

    Implement logging.

--- server.py
+++ server.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 import shutil
-import os
+from datetime import datetime
+from importlib.metadata import version
+import random
+import string
+import traceback
 from pigeon import *
 from description import *
-from flask import Flask, request, send_from_directory
+from flask import Flask, request
+
+def log(uid, lat, lng, error=None):
+    xml = ""
+    for file in shutil.os.listdir("cache/"+uid):
+        if file.endswith('.xml'):
+            xml = "cache/%s/%s"%(uid,file)
+            break
+    libraries = ""
+    for lib in ["osmnx","crossroads-segmentation","crossroads-description"]:
+        libraries += "%s %s\n"%(lib, version(lib))
+    logfile = "DATE : %s\nPOSITION : %s %s\nLIBRARIES : \n%s"%(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), lat, lng, libraries)
+    if error:
+        logfile += "ERROR : \n%s"%error
+    if xml:
+        with open(xml, 'r') as f:
+            content = f.read()
+            logfile += "OSMXML CONTENT : \n%s"%content
+
+    fname = ("error_%s%s.log" if error else "%s%s.log")%(''.join(random.choice(string.ascii_letters) for i in range(10)), datetime.now().timestamp())
+    with open("log/"+fname, 'w') as f:
+        f.write(logfile)
+
 
 # clear folders
-shutil.rmtree("cache", ignore_errors=True), shutil.os.mkdir("cache") 
+shutil.rmtree("cache", ignore_errors=True), shutil.os.mkdir("cache") , shutil.os.makedirs("log", exist_ok=True)
 
 app = Flask(__name__)
 
@@ -24,15 +50,18 @@ def send_pigeon():
     pigeon = PigeonNelson("Crossroads Describer", "Décrire un carrefour proche de sa position", "UTF-8", 0)
 
     if lat and lng and uid:
-        shutil.rmtree("cache/"+uid, ignore_errors=True), shutil.os.mkdir("cache/"+uid) 
+        shutil.rmtree("cache/"+uid, ignore_errors=True), shutil.os.mkdir("cache/"+uid)
+        error = None
         try:
             generateDescription(pigeon, uid, float(lat), float(lng))
         except Exception as e:
-            print(e)
+            error = traceback.format_exc()
             pigeon.setMessage("Erreur lors de la génération de la description. Veuillez essayer un autre carrefour.", "fr", 1)
             pigeon.setGeoJson("{}")
+        
+        log(uid, lat, lng, error)
 
     return pigeon.getJson()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(shutil.os.environ.get("PORT", 8080)))
